8c321b021dcca7e3d3c88b2e0247ca940a40e46d
LegendaryAlexav <axm@dragonangel.com>
2025-01-15 14:51:05 -0500

    Finalized the movement script

--- Assets/Scripts/Mac/PlayerMovement.cs
+++ Assets/Scripts/Mac/PlayerMovement.cs
@@ -6,25 +6,37 @@ public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
     public float speed = 8f;
-    private float jumpForce = 16f;
+    public float jumpForce = 16f;
+    public bool grounded;
     private bool isFacingRight = true;
 
-    [SerializeField] private Rigidbody rb;
-    [SerializeField] private Transform groundCheck;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private BoxCollider2D groundCheck;
     [SerializeField] private LayerMask groundLayer;
 
     private void Update() {
         horizontal = Input.GetAxisRaw("Horizontal");
 
+        if(IsGrounded() && Input.GetButtonDown("Jump")) {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        if(Input.GetButtonUp("Jump") && rb.velocity.y > 0f) {
+            // Slows down the velocity.y so that if you hold the jump button longer it makes you jump higher
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
         CheckFlip();
     }
 
-    private void FixdeUpdate() {
+    private void FixedUpdate() {
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
 
     private bool IsGrounded() {
-        return true;
+        //Check if the box colliders is colliding with the ground layer.
+        grounded = Physics2D.OverlapAreaAll(groundCheck.bounds.min, groundCheck.bounds.max, groundLayer).Length > 0;
+        return grounded;
     }
 
     private void CheckFlip() {
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # 2d-Platformer-WorkShop-uOGDC
- 
+
+This is a tutorial on how to make a 2D Platformer in Unity made for the uOttawa Game Developement Club.
+
+---
+
+Simply install the Git, open Unity Hub (if not already installed go here to install it: https://unity.com/download).
+Click the arrow next to the Open button. Select "Add project from disk", and install all the required installs it will prompt you with.
+Afterwards you are able to run it!
+
+---
+
+To view the youtube tutorial here is the club's Youtube: https://www.youtube.com/@uOttawaGameDevClub
+
+If you use this tutorial for a game, simply just credit us for the help you used!
+
+---
+
+Part1 of the 2D PlatFormer Workshop was made by Axe Miron
